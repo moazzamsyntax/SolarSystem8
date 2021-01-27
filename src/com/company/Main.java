88ad6ddb90b194f9f792c2e4faf2448129293557
moazzamsyntax <moazzamsyntax@gmail.com>
2021-01-26 20:45:23 -0500

    Solar system Deployed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        SolarSystem FactorX =new SolarSystem();
+        FactorX.Sun=1;
+        FactorX.Moon=3;
+        FactorX.Planets=2;
+
     }
 }
